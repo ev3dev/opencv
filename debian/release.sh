668d307b380d2ea9aa098eff14199b5b64dac099
David Lechner <david@lechnology.com>
2018-04-15 20:41:41 -0500

    debian: update release script to build on RPi
    building using QEMU is haning for some reason

--- debian/release.sh
+++ debian/release.sh
@@ -8,10 +8,44 @@ source=$(dpkg-parsechangelog -S Source)
 version=$(dpkg-parsechangelog -S Version)
 distribution=$(dpkg-parsechangelog -S Distribution)
 codename=$(debian-distro-info --codename --${distribution})
+arm_target="robot@ev3dev-rpi3"
 
-OS=debian DIST=${codename} ARCH=armhf pbuilder-ev3dev build
-OS=debian DIST=${codename} ARCH=armel PBUILDER_OPTIONS="--binary-arch" pbuilder-ev3dev build
-OS=raspbian DIST=${codename} ARCH=armhf pbuilder-ev3dev build
+gbp buildpackage -S -us -uc
+
+ssh ${arm_target} "mkdir -p ~/pbuilder-ev3dev/source"
+scp ../${source}_${version}.dsc ${arm_target}:~/pbuilder-ev3dev/source
+scp ../${source}_${version%-*}.orig.tar.gz  ${arm_target}:~/pbuilder-ev3dev/source
+scp ../${source}_${version}.debian.tar.xz ${arm_target}:~/pbuilder-ev3dev/source
+
+ssh -t ${arm_target} "OS=debian DIST=${codename} ARCH=armhf pbuilder-ev3dev dsc-build \
+    ~/pbuilder-ev3dev/source/${source}_${version}.dsc"
+mkdir -p ~/pbuilder-ev3dev/debian/${codename}-armhf
+scp ${arm_target}:~/pbuilder-ev3dev/debian/${codename}-armhf/lmsasm_${version}_armhf.deb \
+    ~/pbuilder-ev3dev/debian/${codename}-armhf/
+scp ${arm_target}:~/pbuilder-ev3dev/debian/${codename}-armhf/${source}_${version}_armhf.changes \
+    ~/pbuilder-ev3dev/debian/${codename}-armhf/
+
+ssh -t ${arm_target} "OS=debian DIST=${codename} ARCH=armel PBUILDER_OPTIONS='--binary-arch' pbuilder-ev3dev dsc-build \
+    ~/pbuilder-ev3dev/source/${source}_${version}.dsc"
+mkdir -p ~/pbuilder-ev3dev/debian/${codename}-armel
+scp ${arm_target}:~/pbuilder-ev3dev/debian/${codename}-armel/lmsasm_${version}_armel.deb \
+    ~/pbuilder-ev3dev/debian/${codename}-armel/
+scp ${arm_target}:~/pbuilder-ev3dev/debian/${codename}-armel/${source}_${version}_armel.changes \
+    ~/pbuilder-ev3dev/debian/${codename}-armel/
+
+ssh -t ${arm_target} "OS=raspbian DIST=${codename} ARCH=armhf pbuilder-ev3dev dsc-build \
+    ~/pbuilder-ev3dev/source/${source}_${version}.dsc"
+mkdir -p ~/pbuilder-ev3dev/raspbian/${codename}-armhf
+scp ${arm_target}:~/pbuilder-ev3dev/raspbian/${codename}-armhf/${source}_${version}.dsc \
+    ~/pbuilder-ev3dev/raspbian/${codename}-armhf/
+scp ${arm_target}:~/pbuilder-ev3dev/raspbian/${codename}-armhf/${source}_${version%-*}.orig.tar.gz \
+    ~/pbuilder-ev3dev/raspbian/${codename}-armhf/
+scp ${arm_target}:~/pbuilder-ev3dev/raspbian/${codename}-armhf/${source}_${version}.debian.tar.xz \
+    ~/pbuilder-ev3dev/raspbian/${codename}-armhf/
+scp ${arm_target}:~/pbuilder-ev3dev/raspbian/${codename}-armhf/lmsasm_${version}_armhf.deb \
+    ~/pbuilder-ev3dev/raspbian/${codename}-armhf/
+scp ${arm_target}:~/pbuilder-ev3dev/raspbian/${codename}-armhf/${source}_${version}_armhf.changes \
+    ~/pbuilder-ev3dev/raspbian/${codename}-armhf/
 
 debsign ~/pbuilder-ev3dev/debian/${codename}-armhf/${source}_${version}_armhf.changes
 debsign ~/pbuilder-ev3dev/debian/${codename}-armel/${source}_${version}_armel.changes
